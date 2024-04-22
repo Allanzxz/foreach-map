11351c336eade28fa50ddf5a37f81d27f71d6282
Allanzxz <allanevangelistabotelho@gmail.com>
2024-04-22 10:49:56 -0400

    finalizada questao 13

---
+++ questao13.js
@@ -0,0 +1,4 @@
+/*
+Questão 13 - Quando você usaria map() em vez de forEach()?    
+*/
+//Você usaria map() em vez de forEach() quando precisar transformar os elementos de um array em um novo array com base em alguma lógica aplicada a cada elemento.
