0bea407b555c8081a419c64e839206436e857ac4
Allanzxz <allanevangelistabotelho@gmail.com>
2024-04-22 10:50:32 -0400

    finalizada questao 18

---
+++ questao18.js
@@ -0,0 +1,4 @@
+/*
+Questão 18 - O que acontece se você chamar map() em um array vazio?   
+*/
+//Se você chamar map() em um array vazio, ele retornará um novo array vazio, já que não há elementos para iterar e aplicar a função de callback
