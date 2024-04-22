20cffe3996351870ffb8adc3382eb156af0822db
Allanzxz <allanevangelistabotelho@gmail.com>
2024-04-22 10:49:52 -0400

    finalizada questao 12

---
+++ questao12.js
@@ -0,0 +1,5 @@
+/*
+Questão 12 - Qual é a diferença entre map() e forEach() em relação ao retorno 
+de valores de uma função de callback?  
+*/
+//A diferença entre map() e forEach() em relação ao retorno de valores de uma função de callback é que o map() retorna um novo array com os resultados da função de callback aplicada a cada elemento do array original, enquanto o forEach() apenas itera sobre os elementos do array original, não retornando um novo array.
