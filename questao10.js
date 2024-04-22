bc76d89771b3ed85aca51e6e5871049feffc7ea9
Allanzxz <allanevangelistabotelho@gmail.com>
2024-04-22 10:49:42 -0400

    finalizada questao 10

---
+++ questao10.js
@@ -0,0 +1,4 @@
+/*
+Questão 10 - É possível modificar o array original com map()?  
+*/
+//Não, o método map() não modifica o array original. Ele retorna um novo array com os resultados da função de callback aplicada a cada elemento do array original. Se você deseja modificar o array original, pode usar métodos como forEach() ou map() em combinação com a atribuição direta a cada elemento do array origin
