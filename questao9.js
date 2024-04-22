8f017372849356865a2e6488011287344a8f5a65
Allanzxz <allanevangelistabotelho@gmail.com>
2024-04-22 10:49:32 -0400

    finalizada questao 9

---
+++ questao9.js
@@ -0,0 +1,4 @@
+/* 
+Questão 09 - Qual é a diferença fundamental entre map() e forEach()?  
+*/
+//A diferença fundamental entre map() e forEach() é que o método map() retorna um novo array com base no resultado da função de callback aplicada a cada elemento do array original, enquanto o método forEach() apenas itera sobre os elementos do array original, não retornando um novo array.
