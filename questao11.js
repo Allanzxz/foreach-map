090cc7c799a578e6b0a57e8fbf27e05c406e204e
Allanzxz <allanevangelistabotelho@gmail.com>
2024-04-22 10:49:47 -0400

    finalizada questao 11

---
+++ questao11.js
@@ -0,0 +1,5 @@
+/*
+Questão 11 - map() sempre retorna um novo array com o mesmo comprimento 
+que o array original?  
+*/
+//Sim, o método map() sempre retorna um novo array com o mesmo comprimento que o array original.
